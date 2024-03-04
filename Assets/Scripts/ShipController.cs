8edb39881071a776339559d8a25ae6fb6dce7c4d
LD <logan.d.m.117@gmail.com>
2024-03-03 20:17:31 -0500

    Update Skybox & ShipController.cs
    The ship Controller is pretty refined and the rotation controls are pretty good now. I need to also setup some kind of rotation speed for it.

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -8,8 +8,10 @@ public class ShipController : MonoBehaviour
     public Camera cam;
     Vector3 input;
     Vector3 moveVector;
+    Vector3 camRotation;
 
     public float moveSpeed = 5f;
+    public float rotationSpeed = 5f;
 
     //Shift slows the player down to zero.
     bool shouldBrake => Input.GetKey(KeyCode.LeftShift);
@@ -19,17 +21,50 @@ public class ShipController : MonoBehaviour
     //LeftCtrl -> Down
     float upAxis => (Input.GetKey(KeyCode.Space) ? 1 : 0) + (Input.GetKey(KeyCode.LeftControl) ? -1 : 0);
 
+    //__I__
+    //_JKL_
+    //For rotation.
+    Vector3 camInput => new Vector3((Input.GetKey(KeyCode.J) ? 1 : 0) + (Input.GetKey(KeyCode.L) ? -1 : 0), (Input.GetKey(KeyCode.I) ? 1 : 0) + (Input.GetKey(KeyCode.K) ? -1 : 0), (Input.GetKey(KeyCode.Q) ? 1 : 0) + (Input.GetKey(KeyCode.E) ? -1 : 0));
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        camRotation = rb.rotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
         input = new Vector3(Input.GetAxisRaw("Horizontal"), upAxis, Input.GetAxisRaw("Vertical"));
-        moveVector = input * moveSpeed;
+        //Vector projection to map inputs to match the orientation of the ship/camera.
+        moveVector = transform.right * input.x + transform.forward * input.z + transform.up * input.y;
+        moveVector = moveVector * moveSpeed;
+        //Rotation
+        //transform.eulerAngles = new Vector3(0, Mathf.Atan2(camInput.x, camInput.y) * 180 / Mathf.PI, 0);
+/*
+        Vector3 translation = camInput.y * transform.up;
+        translation += camInput.x * transform.right;
+        translation.z = 0;
+
+        Vector3 lookDir = Vector3.zero;
+
+        if (translation.magnitude > 0)
+        {
+            lookDir = translation;
+        }
+        else
+        {
+            lookDir = Vector3.zero;
+        }
+
+        if (lookDir.magnitude > 0)
+        {
+            Quaternion camRotation = Quaternion.LookRotation(camInput);
+            rb.MoveRotation(Quaternion.Slerp(transform.rotation, camRotation, Time.deltaTime * rotationSpeed));
+        }*/
+        
+        camRotation += new Vector3(-camInput.y, -camInput.x, camInput.z);
     }
 
     private void FixedUpdate()
@@ -39,10 +74,17 @@ public class ShipController : MonoBehaviour
             //Apply opposite force to stop the ship.
             //Velocity * mass = force.
             rb.AddForce(-rb.velocity * rb.mass);
+            if (rb.velocity.magnitude < 0.1)
+            {
+                rb.velocity = Vector3.zero;
+            }
         }
         else
         {
             rb.AddForce(moveVector);
         }
+
+
+        rb.MoveRotation(Quaternion.Euler(camRotation));
     }
 }
