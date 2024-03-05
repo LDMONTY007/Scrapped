a8516eb99bfb8836d416af004f9fab4d33b1fb6f
LD <logan.d.m.117@gmail.com>
2024-03-05 12:51:19 -0500

    Update RepairMinigame.cs
    I got the repair minigame fully designed and all I need to do now is make it pop up when you interact with damage on the hull.

--- Assets/Scripts/RepairMinigame.cs
+++ Assets/Scripts/RepairMinigame.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class RepairMinigame : MonoBehaviour
 {
     public Slider slider;
 
+    public RectTransform hitAreaTransform;
+    public RectTransform handleTransform;
     public Material sliderMaterial;
 
-    public float leftBound = 0.45f;
-    public float rightBound = 0.55f;
+    public int min = 0;
+    public int max = 500;
 
     public float totalTime = 1f;
     float currentTime = 0f;
     float currentSliderValue = 0f;
     bool doReverse = false;
+
+    private float prevPos = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,9 +27,20 @@ public class RepairMinigame : MonoBehaviour
         
     }
 
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
+
         float timeIncrement = Time.deltaTime;
 
         if (!doReverse)
@@ -59,11 +75,37 @@ public class RepairMinigame : MonoBehaviour
         currentTime += timeIncrement;
 
 
-        if (Input.GetKeyDown(KeyCode.H) && 0.45f <= currentSliderValue && currentSliderValue <= 0.55f)
+        if (Input.GetKeyDown(KeyCode.H) && RectTransformExtensions.Overlaps(handleTransform, hitAreaTransform))
         {
             StartCoroutine(ColorDelay());
         }
+
+        /*if (Input.GetKeyDown(KeyCode.H) && 0.45f <= currentSliderValue && currentSliderValue <= 0.55f)
+        {
+            StartCoroutine(ColorDelay());
+        }*/
     }
+
+    public void SetRandomHitPos()
+    {
+        //TODO:
+        //Do a random.range from the min + hitAreaWidth / 2, and max - hitAreaWidth / 2.
+        //Then set the pos of the hitArea to be that random X position.
+        //anchored position is the actual position relative to the anchor.
+        float currentPos = Random.Range(min + hitAreaTransform.rect.width / 2, max - hitAreaTransform.rect.width / 2);
+
+        //While the distance between the prevPos
+        //is too small generate a new position
+        //regenerate position.
+        while (Mathf.Abs(prevPos - currentPos) < hitAreaTransform.rect.width * 2)
+        {
+            currentPos = Random.Range(min + hitAreaTransform.rect.width / 2, max - hitAreaTransform.rect.width / 2);
+        }
+        hitAreaTransform.anchoredPosition = new Vector2(currentPos, hitAreaTransform.anchoredPosition.y);
+
+        prevPos = currentPos;
+    }
+    
 
     public IEnumerator ColorDelay()
     {
@@ -71,10 +113,13 @@ public class RepairMinigame : MonoBehaviour
         float time = 0.5f;
         while (time > 0)
         {
+            hitAreaTransform.gameObject.GetComponent<Image>().color = Color.cyan;
             sliderMaterial.SetColor("_LineColor", Color.cyan);
             time -= Time.deltaTime;
             yield return null;
         }
+        SetRandomHitPos();
         sliderMaterial.SetColor("_LineColor", Color.red);
+        hitAreaTransform.gameObject.GetComponent<Image>().color = Color.red;
     }
 }
