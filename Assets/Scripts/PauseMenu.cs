f77f50a153a50b38f07b6f33a888ce041c88497e
LDMONTY007 <69697953+LDMONTY007@users.noreply.github.com>
2024-03-10 20:00:52 -0400

    Update UI to include controls.
    I really need to fix the controls for the player and the controls for the camera/ship. They feel horrible.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,11 +5,15 @@ public class PauseMenu : MonoBehaviour
     public bool isPaused;
 
     public GameObject pausePanel;
+    public GameObject playerControlPanel;
+    public GameObject shipControlPanel;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pausePanel.SetActive(false);
+        playerControlPanel.SetActive(false);
+        shipControlPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,11 +29,15 @@ public class PauseMenu : MonoBehaviour
         {
             Time.timeScale = 0f;
             pausePanel.SetActive(true);
+            playerControlPanel.SetActive(true);
+            shipControlPanel.SetActive(true);
         }
         else
         {
             Time.timeScale = 1f;
             pausePanel.SetActive(false);
+            shipControlPanel.SetActive(false);
+            playerControlPanel.SetActive(false);
         }
     }
 }
